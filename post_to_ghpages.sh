f96353909af170c5ac376df926c10431e7213457
Steve <nofailed@gmail.com>
2020-07-28 16:33:11 +0900

    Publishing to master

--- post_to_ghpages.sh
+++ post_to_ghpages.sh
@@ -2,6 +2,8 @@
 
 DIR=$(dirname "$0")
 
+git add -u && git commit -m "Publishing to master" && git push origin master
+
 if [[ $(git status -s) ]]; then
 	echo "The working directory is dirty. Please commit any pending changes."
 	exit 1;
